a402163ef32f846efb79d52ef9b9cb8d54111f76
yuna <2ulkiki0621@gmail.com>
2025-02-01 22:31:59 +0900

    상태표시

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import * as cam from "@mediapipe/camera_utils";
 import { Pose ,POSE_LANDMARKS} from "@mediapipe/pose";
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef,useState }  from 'react';
 import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
 import Webcam from 'react-webcam';
 
 const UserPose = () => {
+  const [poseText,setPoseText] = useState('');
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
   let camera = null;
+
+  // 자세 시간 변수
+  const LEAVE_TIME_SEC = 5; // 자리 비움 설정 시간 (초단위)
+
+  // 자리비움 측정 시작 시간
+  const startTimeRef = useRef(Date.now());
+  // 자리비움 측정 여부
+  const checkLeaveRef = useRef(false);
+  // 자리비움 측정 함수
+  const leaveTimeoutRef = useRef(null);
+  
   const poseDetect = (landmarks) => {
     const LEFT_SHOULDER = landmarks[POSE_LANDMARKS.LEFT_SHOULDER];
     const RIGHT_SHOULDER = landmarks[POSE_LANDMARKS.RIGHT_SHOULDER];
@@ -34,7 +46,7 @@ const UserPose = () => {
     } else if (headPosition <= -0.1) {
       status = "엎드림";
     } 
-    
+    setPoseText(status);
 
     // 키포인트 반환
     const keypoints = Object.values(landmarks).map(lm => ({ x: lm.x, y: lm.y, z: lm.z }));
@@ -75,9 +87,38 @@ const UserPose = () => {
       const { status, keypoints } = poseDetect(results.poseLandmarks);
       console.log(status)
 
+      // 자리비움 측정 중일 경우, 사람이 인식된 경우 -> 자리비움이 아님
+      if (checkLeaveRef.current) {
+        // 자리비움 측정을 종료한다
+        checkLeaveRef.current = false;
+        setPoseText(status);
+
+        // 자리비움 측정 함수가 존재할 경우 해당 함수를 없앰
+        if (leaveTimeoutRef.current) {
+          clearTimeout(leaveTimeoutRef.current);
+          leaveTimeoutRef.current = null;
+        }
+      }
+
       canvasCtx.restore();
     } catch (e) {
-      console.log("자리비움")
+      // console.log("자리비움")
+      // setPoseText('자리비움')
+
+      // checkLeaveRef.current는 checkLeaveRef 안의 변수를 의미함
+      // 자리비움 측정 중일 경우 무시함
+      if (checkLeaveRef.current) return;
+
+      // 자리비움 측졍 여부 변경
+      checkLeaveRef.current = true;
+      // 자리비움 측정 시작 시간 변경
+      startTimeRef.current = Date.now();
+
+      // 자리비움 측정 함수
+      leaveTimeoutRef.current = setTimeout(() => {
+        setPoseText('자리비움');
+        checkLeaveRef.current = false;
+      }, LEAVE_TIME_SEC * 1000); // ms
     }
   }
 
@@ -115,6 +156,21 @@ const UserPose = () => {
 
   return (
     <div className="App">
+      <div
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 15,
+          width: '100%',
+          fontWeight: 'bolder',
+          fontSize: '2rem',
+          color: 'red',
+        }}
+      >{poseText}</div>
       <Webcam
         ref={webcamRef}
         style={{
